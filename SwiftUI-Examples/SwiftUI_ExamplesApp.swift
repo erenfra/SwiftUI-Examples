f7c57943f327d18ac93cfa41becc68ac0ce3a8c2
Renato Oliveira Fraga <rofraga74@gmail.com>
2022-11-22 00:10:26 -0600

    Initial Commit

---
+++ SwiftUI-Examples/SwiftUI_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExamplesApp.swift
+//  SwiftUI-Examples
+//
+//  Created by Renato Oliveira Fraga on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
